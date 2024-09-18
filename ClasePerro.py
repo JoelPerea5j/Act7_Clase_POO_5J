894a7c3ae8c40c5593fe8f0c3b229820840fcd12
JoelPerea5j <a.22308051281272@cbtis128.edu.mx>
2024-09-18 07:32:37 -0600

    Add files via upload

---
+++ ClasePerro.py
@@ -0,0 +1,25 @@
+print("Programacíon POO")
+# Definicion de clase 
+class Perro:
+    # Funciones
+    def morder(self):
+        print("El perro le mordio")
+    def Datos_Perro(self,Nombre,edad):
+        print(f"Nombre: {Nombre} Edad: {edad}")
+
+
+
+# Zona de creacion de objeto
+pitbull=Perro()
+
+
+
+
+# Zona dew uso de objetos
+pitbull.Datos_Perro("Princesa ",4)
+pitbull.morder()
+
+
+
+
+
